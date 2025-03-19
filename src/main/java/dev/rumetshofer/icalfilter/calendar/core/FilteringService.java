dfa80f14905339136553f73d86bd832a3c123fdb
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2025-03-19 19:32:57 +0100

    fix filter bug

--- src/main/java/dev/rumetshofer/icalfilter/calendar/core/FilteringService.java
+++ src/main/java/dev/rumetshofer/icalfilter/calendar/core/FilteringService.java
@@ -5,6 +5,7 @@ import dev.rumetshofer.icalfilter.calendar.core.domain.datacarrier.EventFilterTy
 import net.fortuna.ical4j.model.component.VEvent;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -12,12 +13,14 @@ import java.util.Objects;
 public class FilteringService {
 
     List<VEvent> getEventsToRemove(List<VEvent> events, List<EventFilterData> eventFilters) {
-        List<VEvent> eventsToRemove = events;
+        List<VEvent> eventsToRemove = new ArrayList<>();
         for (EventFilterData eventFilter : eventFilters) {
             if (Objects.requireNonNull(eventFilter.eventFilterType()) == EventFilterType.STARTS_WITH) {
-                eventsToRemove = eventsToRemove.stream()
-                        .filter(event -> event.getSummary().getValue().startsWith(eventFilter.filterValue()))
-                        .toList();
+                eventsToRemove.addAll(
+                        events.stream()
+                            .filter(event -> event.getSummary().getValue().startsWith(eventFilter.filterValue()))
+                            .toList()
+                );
             }
             else {
                 throw new IllegalStateException("Unknown filter type: " + eventFilter.eventFilterType());
